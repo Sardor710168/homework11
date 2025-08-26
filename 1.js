af5ed4a8428ebaaee390f9f04e1ce71ed3f2bbfa
Sardor Xudoyberdiyev <xudoyberdiyevsardor469@gmail.com>
2025-08-27 03:30:30 +0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,22 +1,39 @@
-todoList = {
-    task: [],
+let todoList = {
+    tasks: [],
+
     addTask: function(task) {
-        this.task.push(task);
-        console.log(`Task "${task}` + `" added to the list.`)
+        this.tasks.push(task);
+        console.log(`Task "${task}" added to the list.`);
     },
+
     removeTask: function(task) {
-        const index = this.task.indexOf(task);
+        const index = this.tasks.indexOf(task);
         if (index > -1) {
-            this.task.splice(index, 1);
-            console.log(`Task "${task} ` + `" removed from the list.`)
+            this.tasks.splice(index, 1);
+            console.log(`Task "${task}" removed from the list.`);
         } else {
-            console.log(`Task "${task} ` + `" not found in the list.`)
+            console.log(`Task "${task}" not found in the list.`);
         }
     },
+
     printTasks: function() {
-        console.log("current tasks:");
-        this.task.array.forEach(element => {
-            console.log(element);
-        });
+        if (this.tasks.length === 0) {
+            console.log("No tasks in the list.");
+        } else {
+            console.log("Current tasks:");
+            this.tasks.forEach((element, i) => {
+                console.log(`${i + 1}. ${element}`);
+            });
+        }
     }
-}
+};
+
+todoList.addTask("Kitob o‘qish");
+todoList.addTask("Dars qilish");
+todoList.addTask("Sport bilan shug‘ullanish");
+
+todoList.printTasks();
+
+todoList.removeTask("Dars qilish");
+
+todoList.printTasks();
