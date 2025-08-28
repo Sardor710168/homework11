2e386d989592ae70d92b041e94e8028249a3b5db
Sardor Xudoyberdiyev <xudoyberdiyevsardor469@gmail.com>
2025-08-28 10:31:02 +0500

    Update 5.js

---
+++ 5.js
@@ -0,0 +1,8 @@
+function numInStr(arr) {
+    return arr.filter(str => /[0-9]/.test(str));
+}
+
+console.log(numInStr(["1a", "a", "2b", "b"]));
+console.log(numInStr(["abc", "abc10"]));
+console.log(numInStr(["abc", "ab10c", "a10bc", "bcd"]));
+console.log(numInStr(["this is a test", "test1"]));
